008304581c6dae328ca9b1b0bd6317b5c97228c3
gmmmsheikh <70859834+gmmmsheikh@users.noreply.github.com>
2021-02-25 13:24:49 -0800

    Add files via upload

---
+++ home/gmmmsheikh/fltkBug/bug.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "bug.h"
+
+using namespace std;      
+
+void Bug::move_one_unit()
+{   
+   if (direction == NORTH) { row--; }
+   else if (direction == EAST) { column++; }
+   else if (direction == SOUTH) { row++; }
+   else if (direction == WEST) { column--; }
+}
+
+void Bug::turn_right() 
+{
+   direction++;
+   if (direction == DIRECTIONS) { direction = 0; }
+}
+
+int Bug::get_row() const { return row; }
+int Bug::get_column() const { return column; }
+
+void Bug::reset(){direction = NORTH;}
+
+void Bug::up(){/*TODO*/}
---
+++ home/gmmmsheikh/fltkBug/bug.h
@@ -0,0 +1,29 @@
+#ifndef _BUG_H
+#define _BUG_H
+
+using namespace std;
+
+const int NORTH = 0;
+const int EAST = 1;
+const int SOUTH = 2;
+const int WEST = 3;
+const int DIRECTIONS = 4
+
+class Bug{
+private:
+	int position = 0;
+   int row;
+   int column;
+   int direction; // 0 = North, 1 = East, 2 = South, 3 = West
+
+public:
+	void move_one_unit();
+	void turn_right();
+	int get_row() const;
+	int get_column() const;
+	void rest();
+	void up();
+};
+
+
+#endif
